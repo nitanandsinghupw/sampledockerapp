234984e87ab388d5db6c1292265e6de0a1d6ae26
Anand Singh <nitanandsingh@outlook.com>
2020-03-05 09:40:28 -0500

    done

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace pipelines_dotnet_core_docker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+
+           // for (int i = 0; i < length; i++)
+			//{
+
+			//}
+        }
+    }
+}
